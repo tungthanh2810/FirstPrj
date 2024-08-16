4129e6366ce6cd38897dcae51881a54e900311f3
Lê Thanh Tùng <23656771.tung@student.iuh.edu.vn>
2024-08-16 16:23:16 +0700

    first commit

--- src/welcome.java
+++ src/welcome.java
@@ -1,6 +1,6 @@
 public class welcome {
     public static void main(String[] args) {
         System.out.println("Hello Thanh Tùng");
-        System.out.println("Welcome to DHKTPM19A");
+        System.out.println("Welcome to  DHKTPM19A");
     }
 }
